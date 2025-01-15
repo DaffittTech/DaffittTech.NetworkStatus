957e877ed1c80f4ced33bd11af07422497d631a7
David Covey <coveynet@msn.com>
2025-01-14 22:21:40 -0600

    More refining and refactoring to make this thing more efficient and reliable.

--- DaffittTech.NetworkStatus/NetworkConnection.razor.cs
+++ DaffittTech.NetworkStatus/NetworkConnection.razor.cs
@@ -4,14 +4,37 @@ namespace DaffittTech.NetworkStatus
 {
     public partial class NetworkConnection : ComponentBase
     {
-        [Parameter] public string Status { get; set; } = "Checking...";
+        [Inject] protected NetworkService NetworkService { get; set; }
+        [Parameter] public int? Interval { get; set; } = null; // timing between checks in seconds.
+
+        private string ConnectionStatus { get; set; } = "Checking...";
+        private bool NetworkStatus { get; set; }
         private string AlertColor { get; set; } = "alert-secondary";
 
-        protected override Task OnParametersSetAsync()
+        protected async override Task OnParametersSetAsync()
         {
-            Status = string.IsNullOrEmpty(Status) || Status.ToLower().Contains("checking") ? "Checking..." : Status.ToLower() == "online" ? "Online" : "Offline";
-            AlertColor = string.IsNullOrEmpty(Status) || Status.ToLower().Contains("checking") ? "alert-secondary" : Status.ToLower() == "online" ? "alert-success" : "alert-danger";
-            return base.OnParametersSetAsync();
+            await NetworkService.MonitorStatus(Interval);
+            await base.OnParametersSetAsync();
+        }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                NetworkService.OnNetworkStatusChanged += UpdateNetworkStatus;
+                await NetworkService.Initialize();
+                await NetworkService.CheckStatusAsync();
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        private void UpdateNetworkStatus(bool status)
+        {
+            NetworkStatus = status;
+            ConnectionStatus = NetworkStatus == true ? "Online" : "Offline";
+            ConnectionStatus = string.IsNullOrEmpty(ConnectionStatus) || ConnectionStatus.ToLower().Contains("checking") ? "Checking..." : ConnectionStatus.ToLower() == "online" ? "Online" : "Offline";
+            AlertColor = string.IsNullOrEmpty(ConnectionStatus) || ConnectionStatus.ToLower().Contains("checking") ? "alert-secondary" : ConnectionStatus.ToLower() == "online" ? "alert-success" : "alert-danger";
+            StateHasChanged();
         }
     }
 }
